2a607a82de5c7d173c733a8f294325510aefd97f
Calpurniax <sofia.gracia@gmail.com>
2022-11-28 08:57:16 +0100

    create json file render json data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
 import '../styles/App.scss';
+import contact from '../services/api.json'
+import { useState } from 'react';
 
 function App() {
+  const [adalabers, setAdalabers] = useState(contact.results)
+  console.log(adalabers)
+  const renderAdalaber = () => {
+    return adalabers.map((each) => (
+      <tr key={each.id}>
+        <td>{each.name}</td>
+        <td>{each.counselor}</td>
+        <td>{each.speciality}</td>
+      </tr>))
+  }
   return (
     <div className='App'>
-      <h1>madre mía</h1>
+      <header><h1>Adalabers</h1></header>
+      <table className="table">
+        {/* <!-- Fila de cabecera --> */}
+        <thead><tr>
+          <th>Nombre</th>
+          <th>Tutora</th>
+          <th>Especialidad</th>
+        </tr></thead>
+        <tbody>
+          {renderAdalaber()}
+        </tbody>
+      </table>
     </div>
   );
 }
